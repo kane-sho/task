13a51b5658df3d29a6f8dd2f327a9b969830e074
kane-sho <p.g.r.s.k.5.5@gmail.com>
2021-04-18 16:30:13 +0900

    バリデーション実装

--- config.php
+++ config.php
@@ -4,5 +4,8 @@ define('DSN', 'mysql:host=db;dbname=task_app;cahrset=utf8');
 define('USER', 'testuser');
 define('PASSWORD', '9999');
 
+// エラーメッセージを定数として定義
+define('MSG_TITLE_REQUIRED', 'タスク名を入力して下さい');
+
 // ステータスを定数として定義
 define('TASK_STATUS_NOTYET', 'notyet');
