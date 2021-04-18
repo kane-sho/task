56bbc3eab82ca8e326fc298423ca0c7c734fa651
kane-sho <p.g.r.s.k.5.5@gmail.com>
2021-04-18 15:20:43 +0900

    first commit

---
+++ edit.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Tasks</title>
+    <link rel="stylesheet" href="https://unpkg.com/ress@3.0.0/dist/ress.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2>タスク更新</h2>
+        <form action="" method="post">
+            <input type="text" name="title">
+            <input type="submit" value="更新" class="btn sumit-btn">
+        </form>
+        <a href="index.php" class="btn return-btn">戻る</a>
+    </div>
+</body>
+</html>
